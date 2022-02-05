11f54bc273282eb07e215dddefd7681d4829f50e
Jordan Haines <jordan@factor3.co.uk>
2022-02-05 12:07:39 +0000

    Base file added

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+var site = {
+	init: function() {
+
+		/* Navigation */
+		jQuery('.responsive-menu').click(function(e) {
+			e.preventDefault();
+			jQuery('header nav').slideToggle();
+		}); 
+		/* END - JS nav slide toggle */
+	  
+		/*** Remove inline styles at media queries ***/
+		jQuery(window).resize(function() {
+			if (window.innerWidth > 640) {
+				jQuery("header nav").removeAttr("style");
+			}
+		}); 
+		/* END - JS to remove inline styles to nav */
+	  
+		jQuery(".responsive-menu").click(function() {
+			jQuery(".down").toggleClass('downstart');
+			jQuery(".fade").toggleClass("fadestart");
+			jQuery(".up").toggleClass("upstart");
+		});
+
+    }
+};
+
+jQuery(document).ready(function(){ site.init(); });
